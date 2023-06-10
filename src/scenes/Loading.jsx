352e601bc5ffda50fdad43c622e902c247fca3e8
Chakravarthy-7 <chakravarthy.8328@gmai.com>
2023-06-10 21:16:13 +0530

    loader changed

--- src/scenes/Loading.jsx
+++ src/scenes/Loading.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./loading.css";
+
 const Loading = () => {
   return (
-    <div className="loading">
-      <div className="center">
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
+    <div className="container">
+      <div className="loader-container">
+        <div className="loader">
+          <div className="loader-square"></div>
+          <div className="loader-square"></div>
+          <div className="loader-square"></div>
+          <div className="loader-square"></div>
+          <div className="loader-square"></div>
+          <div className="loader-square"></div>
+          <div className="loader-square"></div>
+        </div>
       </div>
     </div>
   );
